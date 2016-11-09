a30835e6fa64260ced7780d0fcc62b2d5ca3921d
lanking520 <lanking520@gmail.com>
2016-11-08 20:14:22 -0500

    Add halls

--- webserver/routes/coach.py
+++ webserver/routes/coach.py
@@ -8,3 +8,16 @@ def coach():
     return render_template("coach.html")
 
 
+@routes.route('/coach/gethalls',methods=['GET'])
+def halls():
+    temp = {'location':'','name':'','capacity':''}
+    halls_data=[]
+    cursor = g.conn.execute('SELECT * from hall;')
+    for row in cursor:
+        temp['location'] = row[0]
+        temp['name'] = row[1]
+        temp['capacity'] = row[2]
+        halls_data.append(temp)
+    resp = Response(response=json.dumps(halls_data),status=200, mimetype="application/json")
+    return(resp)
+    
