65b2c204137df6abd64af8d3a7234cceababadf5
lanking520 <lanking520@gmail.com>
2016-11-13 02:31:14 -0500

    Add get to the member

--- webserver/routes/member.py
+++ webserver/routes/member.py
@@ -3,6 +3,39 @@ from . import routes
 from server import *
 import json
 
+def table_name_extracter(name):
+	cursor = g.conn.execute('SELECT column_name FROM information_schema.columns WHERE table_name= %s', name)
+	names = []
+	for row in cursor:
+		names.append(row[0])
+	return names
+
 @routes.route('/member')
 def member():
     return render_template("member.html")
+
+@routes.route('/member/equipment',methods=['GET'])
+def getequipment():
+	result = []
+	table_names = table_name_extracter('equipment')
+	cursor = g.conn.execute('SELECT * from equipment;');
+	for row in cursor:
+		temp = {}
+		for i in range(len(table_names)):            
+			temp[table_names[i]]=row[i]
+		result.append(temp)
+	resp = Response(response=json.dumps(result),status=200, mimetype="application/json")
+	return(resp)
+
+@routes.route('/member/courses',methods=['GET'])
+def getmembercourse():
+	result = []
+	table_names = table_name_extracter('course')
+	cursor = g.conn.execute('SELECT * from course;');
+	for row in cursor:
+		temp = {}
+		for i in range(len(table_names)):            
+			temp[table_names[i]]=row[i]
+		result.append(temp)
+	resp = Response(response=json.dumps(result),status=200, mimetype="application/json")
+	return(resp)
